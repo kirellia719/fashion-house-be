7b966f3b878ad69d37c1866be5e8edbf1de482e7
Khanh <kirellia719@gmail.com>
2024-04-20 18:55:17 +0700

    verce

--- routes/UploadRouter.js
+++ routes/UploadRouter.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const uploadCloud = require("../cloudinary.config");
 
 router.post("/", uploadCloud.single("file"), async (req, res) => {
+  console.log(req.file);
   res.json(req.file?.path);
 });
 
